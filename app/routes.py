1b0d991067d1b809f13d704c192ef90a62e18926
Cristian Bou <cbougeno@everis.com>
2018-05-28 15:01:50 +0200

    Code Hello World

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,6 @@
-from flask import render_template
 from app import app
-
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Cris'}
-    posts = [
-        {
-            'author': {'username': 'Marina'},
-            'body': 'Beautiful day in Towerplane!'
-        },
-        {
-            'author': {'username': 'Pablo'},
-            'body': 'I wanna play La Caja!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+	return "Hello World!"
